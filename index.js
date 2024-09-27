538bd7a9dfe45793d8bf434ac967c071be8be3b7
sumoncse19 <mdsumoncse19@gmail.com>
2024-09-27 21:00:14 +0600

    update

--- index.js
+++ index.js
@@ -20,54 +20,48 @@ const { MongoClient, ServerApiVersion } = require("mongodb");
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_USER}@cluster0.pqw80zr.mongodb.net/generate-pdf?retryWrites=true&w=majority&appName=Cluster0`;
 const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
 const run = async () => {
   try {
-    const dataBase = client.db("generate-pdf");
-  } finally {
-  }
-};
+    const dataBase = await client.db("generate-pdf");
+    console.log(dataBase, "database connected");
 
-run().catch((err) => console.log(err));
+    // Initialize Puppeteer function
+    async function initializePuppeteer() {
+      puppeteer = await import("puppeteer");
+      executablePath = puppeteer.executablePath();
+    }
 
-app.get("/", (req, res) => {
-  res.send("Hello World!");
-});
+    // PDF generation route
+    app.post("/generate-pdf", async (req, res) => {
+      await initializePuppeteer();
 
-// Initialize Puppeteer function
-async function initializePuppeteer() {
-  puppeteer = await import("puppeteer");
-  executablePath = puppeteer.executablePath();
-}
+      const { html } = req.body;
 
-// PDF generation route
-app.post("/generate-pdf", async (req, res) => {
-  await initializePuppeteer();
+      if (!html) {
+        console.error("HTML content is missing");
+        return res.status(400).send("HTML content is required");
+      }
 
-  const { html } = req.body;
+      const htmlContent = decodeURIComponent(html);
+      let browser;
 
-  if (!html) {
-    console.error("HTML content is missing");
-    return res.status(400).send("HTML content is required");
-  }
+      try {
+        browser = await puppeteer.launch({
+          args: ["--no-sandbox", "--disable-setuid-sandbox"], // Adjust args based on environment
+          defaultViewport: null,
+          executablePath, // Will use the correct path based on the environment
+          ignoreHTTPSErrors: true,
+          headless: true,
+        });
 
-  const htmlContent = decodeURIComponent(html);
-  let browser;
-
-  try {
-    browser = await puppeteer.launch({
-      args: ["--no-sandbox", "--disable-setuid-sandbox"], // Adjust args based on environment
-      defaultViewport: null,
-      executablePath, // Will use the correct path based on the environment
-      ignoreHTTPSErrors: true,
-      headless: true,
-    });
-
-    const page = await browser.newPage();
-    await page.setContent(
-      `
+        const page = await browser.newPage();
+        await page.setContent(
+          `
       <html>
         <head>
           <link href="https://cdn.jsdelivr.net/npm/tailwindcss@2.2.19/dist/tailwind.min.css" rel="stylesheet">
@@ -84,40 +78,52 @@ app.post("/generate-pdf", async (req, res) => {
         </body>
       </html>
     `,
-      { waitUntil: "networkidle0" }
-    );
+          { waitUntil: "networkidle0" }
+        );
 
-    await page.addStyleTag({
-      content: `
+        await page.addStyleTag({
+          content: `
       body {
           -webkit-print-color-adjust: exact;
           print-color-adjust: exact;
         }
       `,
+        });
+
+        // await page.emulateMediaType("screen");
+
+        const pdf = await page.pdf({
+          format: "A4",
+          printBackground: true,
+        });
+
+        res.setHeader("Content-Type", "application/pdf");
+        res.setHeader(
+          "Content-Disposition",
+          'attachment; filename="activity.pdf"'
+        );
+        res.setHeader("Content-Length", pdf.length);
+        res.setHeader("Access-Control-Allow-Origin", "*");
+        res.end(pdf, "binary");
+      } catch (error) {
+        console.error("Error generating PDF:", error);
+        res.status(500).send("Error generating PDF");
+      } finally {
+        if (browser) {
+          await browser.close();
+        }
+      }
     });
-
-    // await page.emulateMediaType("screen");
-
-    const pdf = await page.pdf({
-      format: "A4",
-      printBackground: true,
-    });
-
-    res.setHeader("Content-Type", "application/pdf");
-    res.setHeader("Content-Disposition", 'attachment; filename="activity.pdf"');
-    res.setHeader("Content-Length", pdf.length);
-    res.setHeader("Access-Control-Allow-Origin", "*");
-    res.end(pdf, "binary");
-  } catch (error) {
-    console.error("Error generating PDF:", error);
-    res.status(500).send("Error generating PDF");
   } finally {
-    if (browser) {
-      await browser.close();
-    }
   }
+};
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
 });
 
+run().catch(console.dir);
+
 app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+  console.log(`Server running at on port:${port}`);
 });
